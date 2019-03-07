23614776cac73f21a4c55d1a959d514125f1e0d9
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-03-07 22:14:32 +0100

    Initial Commit

---
+++ webview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview
+//
+//  Created by Ievgeniia Pavliuchyk on 07/03/2019.
+//  Copyright © 2019 Ievgeniia Pavliuchyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
