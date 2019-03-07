21d941892b6a692f2f6e3a33c8e59fac2b8e2c9a
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-03-07 22:35:00 +0100

    Web  kit added to project

--- webViewApp/ViewController.swift
+++ webViewApp/ViewController.swift
@@ -16,8 +16,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func back(_ sender: Any) {
+    }
+    
+    @IBAction func forward(_ sender: Any) {
+    }
+    
+    @IBAction func close(_ sender: Any) {
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+    }
 }
 
